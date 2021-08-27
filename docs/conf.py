cc291c60b4989e8e0d08bae88a746a5b8e2cecdc
Kay Robbins <1189050+VisLab@users.noreply.github.com>
2021-08-27 17:45:27 -0500

    Updated the internal link format as an experiment

--- docs/conf.py
+++ docs/conf.py
@@ -31,6 +31,7 @@ release = '8.0.0'
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
 extensions = ["myst_parser"]
+myst_heading_anchors = 2
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
