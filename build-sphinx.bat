5380de3380dbe7fd1f1c533d43a063474297a183
Kay Robbins <1189050+VisLab@users.noreply.github.com>
2025-07-16 08:30:17 -0500

    Removed the pdf generation

--- build-sphinx.bat
+++ build-sphinx.bat
@@ -7,24 +7,24 @@ echo.
 echo Building HTML documentation...
 sphinx-build -b html docs/source docs/_build/html
 
-echo.
-echo Building PDF documentation...
-sphinx-build -b latex docs/source docs/_build/latex
+rem echo.
+rem echo Building PDF documentation...
+rem sphinx-build -b latex docs/source docs/_build/latex
 
-echo.
-echo Converting LaTeX to PDF...
-cd docs\_build\latex
-pdflatex HEDSpecification.tex
-pdflatex HEDSpecification.tex
-cd ..\..\..
+rem echo.
+rem echo Converting LaTeX to PDF...
+rem cd docs\_build\latex
+rem pdflatex HEDSpecification.tex
+rem pdflatex HEDSpecification.tex
+rem cd ..\..\..
 
-echo.
-echo Copying PDF to pdfs directory...
-if not exist "docs\_build\pdfs" mkdir "docs\_build\pdfs"
-copy "docs\_build\latex\HEDSpecification.pdf" "docs\_build\pdfs\"
+rem echo.
+rem echo Copying PDF to pdfs directory...
+rem if not exist "docs\_build\pdfs" mkdir "docs\_build\pdfs"
+rem copy "docs\_build\latex\HEDSpecification.pdf" "docs\_build\pdfs\"
 
-echo.
-echo Documentation built successfully!
-echo HTML files are in: docs/_build/html/
-echo PDF file is in: docs/_build/pdfs/HEDSpecification.pdf
-echo Open docs/_build/html/index.html in your browser to view locally.
+rem echo.
+rem echo Documentation built successfully!
+rem echo HTML files are in: docs/_build/html/
+rem echo PDF file is in: docs/_build/pdfs/HEDSpecification.pdf
+rem echo Open docs/_build/html/index.html in your browser to view locally.
